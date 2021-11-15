4f8295ca5fd56207d007e99b799660ce0c1bae8e
João Carlos Vernetti dos Santos <jcvernetti@gmail.com>
2021-11-15 16:19:18 -0300

    Foram criados o método getLista na classe ContatoDao, a classe main TestaLista para imprimir a lista de contatos.

---
+++ fj21-jdbc/src/br/com/caelum/jdbc/teste/TestaLista.java
@@ -0,0 +1,28 @@
+package br.com.caelum.jdbc.teste;
+
+import java.util.List;
+
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaLista {
+
+	public static void main(String[] args) {
+
+		// Cria um ContatoDao
+		ContatoDao dao = new ContatoDao();
+
+		// Lista os contatos com o DAO:
+		List<Contato> contatos = dao.getLista();
+
+		// Iterar nessa lista e imprima as informações dos contatos:
+		for (Contato contato : contatos) {
+			System.out.println("Nome: " + contato.getNome());
+			System.out.println("Email: " + contato.getEmail());
+			System.out.println("Endereço: " + contato.getEndereco());
+			System.out.println("Data de Nascimento: " + contato.getDataNascimento().getTime() + "\n");
+		}
+
+	}
+
+}
